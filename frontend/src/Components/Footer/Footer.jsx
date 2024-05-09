fa801918783b3f46961b0046bd5aeb10c9f741b9
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-05-10 02:44:30 +0530

    Styles: CSS update

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../Assets/logo_big.png'
+import footer_logo from '../Assets/logo1.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pinterest_icon from '../Assets/pinterest_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer-logo">
-                <img src={footer_logo} alt="" />
+                <img src={footer_logo} alt="" className='big-logo'/>
                 <p>GREENCART</p>
         </div>
         <ul className="footer-links">
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo1.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
@@ -22,15 +22,15 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="nav-logo" onClick={mainPageRedirection}>
-            <img src={logo} alt="" />
+            <img src={logo} alt="" className='small-logo' />
             <p>GREENCART</p>
         </div>
         <img className='nav-dropdown' src={nav_dropdown} onClick={dropdown_toggle} alt="" />
         <ul ref={menuRef} className="nav-menu">
-            <li onClick={()=>{setMenu('shop')}}><Link style = {{textDecoration:'none'}} to='/'>Shop</Link>{menu==='shop'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('men')}}><Link style = {{textDecoration:'none'}} to='/men'>Men</Link>{menu==='men'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('women')}}><Link style = {{textDecoration:'none'}} to='/women'>Women</Link>{menu==='women'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('kid')}}><Link style = {{textDecoration:'none'}} to='/kid'>Kids</Link>{menu==='kid'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('shop')}} className='category'><Link style = {{textDecoration:'none', color: 'black'}} to='/'>Shop</Link>{menu==='shop'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('men')}}className='category'><Link style = {{textDecoration:'none', color: 'black'}} to='/men'>Men</Link>{menu==='men'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('women')}}className='category'><Link style = {{textDecoration:'none', color: 'black'}} to='/women'>Women</Link>{menu==='women'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('kid')}}className='category'><Link style = {{textDecoration:'none', color: 'black'}} to='/kid'>Kids</Link>{menu==='kid'?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             {localStorage.getItem('auth-token')
