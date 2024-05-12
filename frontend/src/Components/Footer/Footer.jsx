63478f01c9a4d684ac006bc17d9b497a9a63201e
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-05-13 04:10:37 +0530

    Added: Social Media Links and Icons and Modified: carousel1

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Footer.css'
 import footer_logo from '../Assets/logo1.png'
 import instagram_icon from '../Assets/instagram_icon.png'
-import pinterest_icon from '../Assets/pinterest_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import gmail_icon from '../Assets/gmail.png'
 
 const Footer = () => {
   return (
@@ -12,19 +12,24 @@ const Footer = () => {
                 <img src={footer_logo} alt="" className='big-logo'/>
         </div>
         <ul className="footer-links">
-            <li>Company</li>
             <li>About</li>
             <li>Contact</li>
         </ul>
         <div className="footer-social-icon">
             <div className="footer-icon-container">
-                <img src={instagram_icon} alt="" />
+                <a href='https://www.instagram.com/greencart_officials/?hl=en' target='blank'>
+                    <img src={instagram_icon} alt="" />
+                </a>
             </div>
             <div className="footer-icon-container">
-                <img src={pinterest_icon} alt="" />
+                <a href="https://chat.whatsapp.com/H9IdpnvFdSr9JrIL6PlxzS" target='blank'>
+                    <img src={whatsapp_icon} alt="" />
+                </a>
             </div>
             <div className="footer-icon-container">
-                <img src={whatsapp_icon} alt="" />
+                <a href="mailto:greencartofficials@gmail.com" target='blank'>
+                    <img src={gmail_icon} alt="" />
+                </a>
             </div>
         </div>
         <div className="footer-copyright">
